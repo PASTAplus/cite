89ec1c76c4b5a870bf726cc16620a7cc4c8179d4
Mark Servilla <mark.servilla@gmail.com>
2020-01-11 07:50:41 -0700

    Create runnable minimum environment

--- webapp/run.py
+++ webapp/run.py
@@ -16,6 +16,7 @@ from flask import abort, Flask, request, send_file
 import requests
 
 from webapp.config import Config
+from webapp.eml import Eml
 
 logger = daiquiri.getLogger("run.py: " + __name__)
 
@@ -24,24 +25,40 @@ app.config.from_object(Config)
 
 
 @app.route("/cite")
-def hello_world():
-    return "Hello on Wheels!"
+@app.route("/cite/help")
+def help():
+    return "help!"
 
 
 @app.route("/cite/<pid>")
 def cite(pid=None):
     env = request.args.get("env")
-    format = request.args.get("format")
-
-    if pid is None:
-        return "Help!"
+    accept = request.args.get("format")
+    # TODO token = request.args.get("token")
     try:
-        # response = citation(pid=pid, env=env, format=format)
-        pass
-        return "response"
-    except requests.exceptions.ConnectionError as e:
+        response = citation(pid=pid, env=env, accept=accept)
+        return response
+    except Exception as e:
         logger.error(e)
         abort(400)
+
+
+def citation(pid: str = None, env: str = "p", accept: str = "text/html") -> str:
+    c = None
+    scope, identifier, revision = pid_triple(pid)
+    url = f"{Config.PASTA_P}/metadata/eml/{scope}/{identifier}/{revision}"
+    r = requests.get(url)
+    eml = Eml(r.text)
+    c = f"Your package identifier is {pid}!"
+    return c
+
+
+def pid_triple(pid: str) -> tuple:
+    _ = pid.strip().split(".")
+    scope = str(_[0])
+    identifier = int(_[1])
+    revision = int(_[2])
+    return scope, identifier, revision
 
 
 if __name__ == "__main__":
