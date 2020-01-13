4666e9b9f8cf59cdf737d340ec1a750d698c2818
James Brunt <jwbrunt@gmail.com>
2020-01-13 15:40:14 -0700

    refactor for creator object

--- webapp/eml.py
+++ webapp/eml.py
@@ -31,76 +31,39 @@ def clean(text):
     return " ".join(text.split())
 
 
+class Creator(object):
+    def __init__(self, creator):
+        self._creator = creator
+        self._surname = self._get_surname()
+        self._given_name = None
+        self._org_name = None
+
+    def _get_surname(self):
+        surname = None
+        _ = self._creator.find(".//individualName/surName")
+        if _ is not None:
+            surname = clean(_.text)
+        return surname
+
+    @property
+    def surname(self):
+        return self._surname
+
+
 class Eml(object):
     def __init__(self, eml: str):
         self._eml = eml.encode("utf-8")
         self._root = etree.fromstring(self._eml)
-        self._abstract = self._get_abstract()
-        self._creator = self._get_creator_surname()
-        self._funding = self._get_funding()
-        self._geographic_coverage = self._get_geographic_coverage()
-        self._intellectual_rights = self._get_intellectual_rights()
-        self._keywords = self._get_keywords()
         self._title = self._get_title()
+        self._creators = self._get_creators()
 
-    def _get_abstract(self):
-        abstract = None
-        _ = self._root.find(".//dataset/abstract")
-        if _ is not None:
-            abstract = clean(_.xpath("string()"))
-        return abstract
-
-    def _get_creator_surname(self):
-        creator = None
-        _ = self._root.findall(".//dataset/creator/individualName/surName")
-        if len(_) > 0:
-            creator = list()
-            for surname in _:
-                creator.append(clean(surname.text))
-        return creator
-
-    def _get_funding(self):
-        funding = None
-        _ = self._root.find(".//dataset/project/funding")
-        if _ is not None:
-            funding = clean(_.xpath("string()"))
-        return funding
-
-    def _get_geographic_coverage(self):
-        gc = None
-        _ = self._root.find(".//dataset/coverage/geographicCoverage")
-        if _ is not None:
-            gc = dict()
-            gc["description"] = clean(_.find(".//geographicDescription").text)
-            gc["west"] = clean(
-                _.find(".//boundingCoordinates/westBoundingCoordinate").text
-            )
-            gc["east"] = clean(
-                _.find(".//boundingCoordinates/eastBoundingCoordinate").text
-            )
-            gc["north"] = clean(
-                _.find(".//boundingCoordinates/northBoundingCoordinate").text
-            )
-            gc["south"] = clean(
-                _.find(".//boundingCoordinates/southBoundingCoordinate").text
-            )
-        return gc
-
-    def _get_intellectual_rights(self):
-        ir = None
-        _ = self._root.find(".//dataset/intellectualRights")
-        if _ is not None:
-            ir = clean(_.xpath("string()"))
-        return ir
-
-    def _get_keywords(self):
-        keywords = None
-        _ = self._root.findall(".//dataset/keywordSet/keyword")
-        if len(_) > 0:
-            keywords = list()
-            for keyword in _:
-                keywords.append(clean(keyword.text))
-        return keywords
+    def _get_creators(self):
+        creators = list()
+        _ = self._root.findall(".//dataset/creator")
+        for c in _:
+            creator = Creator(c)
+            creators.append(creator)
+        return creators
 
     def _get_title(self):
         title = ""
@@ -110,28 +73,8 @@ class Eml(object):
         return title
 
     @property
-    def abstract(self):
-        return self._abstract
-
-    @property
-    def creator(self):
-        return self._creator
-
-    @property
-    def funding(self):
-        return self._funding
-
-    @property
-    def geographic_coverage(self):
-        return self._geographic_coverage
-
-    @property
-    def intellectual_rights(self):
-        return self._intellectual_rights
-
-    @property
-    def keywords(self):
-        return self._keywords
+    def creators(self):
+        return self._creators
 
     @property
     def title(self):
