bfd02976ff8c550ed0b35581b443020c3887f4e7
Mark Servilla <mark.servilla@gmail.com>
2020-01-22 16:58:32 -0700

    Add placeholder module for citation creation

---
+++ webapp/citation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+:Mod: citation
+
+:Synopsis:
+
+:Author:
+    servilla
+
+:Created:
+    1/21/20
+"""
+import daiquiri
+
+logger = daiquiri.getLogger(__name__)
+
+
