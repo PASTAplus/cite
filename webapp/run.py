e464f0be9766ef51d5a3e41ac4a93e3306c90667
Mark Servilla <mark.servilla@gmail.com>
2020-01-23 23:02:36 -0700

    Use citation module

--- webapp/run.py
+++ webapp/run.py
@@ -13,19 +13,18 @@
 """
 import logging
 import os
-import json
 
 import daiquiri
 from flask import abort, Flask, request, send_file
-import requests
 
+from webapp.citation import Citation
 from webapp.config import Config
 from webapp.eml import Eml
 
 cwd = os.path.dirname(os.path.realpath(__file__))
 logfile = cwd + "/run.log"
 daiquiri.setup(level=logging.INFO, outputs=(daiquiri.output.File(logfile),
-                                            "stdout"))
+                                            "stderr"))
 logger = daiquiri.getLogger("run.py: " + __name__)
 
 
@@ -42,22 +41,16 @@ def help():
 @app.route("/cite/<pid>")
 def cite(pid=None):
     env = request.args.get("env")
-    accept = request.args.get("format")
+    accept = request.headers.get("Accept")
     # TODO token = request.args.get("token")
+
     try:
-        response = f"{pid}"
+        citation = Citation(pid=pid, env=env, accept=accept)
+        response = citation.citation
         return response
     except Exception as e:
         logger.error(e)
         abort(400)
-
-
-def pid_triple(pid: str) -> tuple:
-    _ = pid.strip().split(".")
-    scope = str(_[0])
-    identifier = int(_[1])
-    revision = int(_[2])
-    return scope, identifier, revision
 
 
 if __name__ == "__main__":
