3d222a2e974030ce9ef78137f61f11fb23859bd2
Mark Servilla <mark.servilla@gmail.com>
2020-01-25 22:25:33 -0700

    Add citation test module

---
+++ tests/test_citation.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+:Mod: test_citation
+
+:Synopsis:
+
+:Author:
+    servilla
+
+:Created:
+    1/25/20
+"""
+import json
+
+import pytest
+
+from webapp.citation import Citation
+
+
+def test_citation():
+    pid = "knb-lter-nin.1.1"
+    env = "production"
+    style = "EDI"
+    accept = "test/html"
+    citation = Citation(pid, env, style, accept)
+    assert isinstance(citation, Citation)
+    assert citation.base is not None
+    print(json.dumps(citation.base, indent=2))
+    print(citation.stylized)
