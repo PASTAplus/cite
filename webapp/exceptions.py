852c11f4c686d61cc3d7663629ba4b81867447b1
Mark Servilla <mark.servilla@gmail.com>
2020-01-27 15:19:45 -0700

    Add exception module

---
+++ webapp/exceptions.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+:Mod: exceptions
+
+:Synopsis:
+
+:Author:
+    servilla
+
+:Created:
+    1/27/20
+"""
+
+class StyleError(Exception):
+    """Raised when a citation style error occurs
+    Args:
+        msg (str): explanation of the error
+    """
