201ac863da003281e1c76a16832749430eecb189
Mark Servilla <mark.servilla@gmail.com>
2020-01-28 10:19:08 -0700

    Embed terminating periods into stylized elements; add DRYAD style

--- webapp/style.py
+++ webapp/style.py
@@ -17,7 +17,6 @@ import pendulum
 
 
 from webapp.exceptions import StyleError
-from webapp.format import Formatter
 from webapp.utils import doi_url
 from webapp.utils import initials
 from webapp.utils import pub_year
@@ -38,68 +37,6 @@ class Stylizer(object):
         else:
             msg = f"Unrecognized style requested: {style}"
             raise StyleError(msg)
-
-
-def esip(citation: dict) -> dict:
-
-    stylized = dict()
-
-    now = (pendulum.now("UTC")).format("D MMM YYYY", formatter="alternative")
-
-    individuals = _individuals(citation["authors"])
-    organizations = _organization(citation["authors"])
-    positions = _positions(citation["authors"])
-
-    names = list()
-    for individual in individuals:
-        given_name_initials = initials(individual[0])
-        individual[0] = given_name_initials
-        name = f"{individual[0]} {individual[1]}"
-        names.append(name.strip())
-    individuals = names
-
-    # Reverse lead author name
-    if len(individuals) > 0:
-        name = individuals[0].split(" ")
-        if len(name) == 2:
-            individuals[0] = f"{name[1]}, {name[0]}"
-
-    authors = individuals + organizations
-    if len(authors) > 0:
-        if len(authors) > 1:
-            authors[-1] = f"and {authors[-1]}"
-        if len(authors) == 2:
-            stylized["authors"] = " ".join(authors)
-        elif len(authors) > 2:
-            stylized["authors"] = ", ".join(authors)
-        else:
-            author = authors[0].rstrip(".")
-            stylized["authors"] = author
-    else:
-        authors = positions
-        if len(authors) > 1:
-            authors[-1] = f"and {authors[-1]}"
-        if len(authors) == 2:
-            stylized["authors"] = " ".join(authors)
-        elif len(authors) > 2:
-            stylized["authors"] = ", ".join(authors)
-        else:
-            author = authors[0].rstrip(".")
-            stylized["authors"] = author
-
-    stylized["pub_year"] = pub_year(citation["pubdate"])
-    stylized["title"] = citation["title"]
-    stylized["version"] = f"Version {citation['version']}"
-    stylized["publisher"] = citation["publisher"]
-    stylized["doi"] = doi_url(citation["doi"])
-    stylized["accessed"] = f"Accessed {now}"
-
-    return stylized
-
-
-styles = {
-    "ESIP": esip
-}
 
 
 def _individuals(authors: list) -> list:
@@ -136,3 +73,117 @@ def _positions(authors: list) -> list:
             for position_name in position_names:
                 positions.append(position_name)
     return positions
+
+
+def dryad(citation: dict) -> dict:
+
+    stylized = dict()
+
+    individuals = _individuals(citation["authors"])
+    organizations = _organization(citation["authors"])
+    positions = _positions(citation["authors"])
+
+    names = list()
+    for individual in individuals:
+        given_name_initials = initials(individual[0], dot=False)
+        individual[0] = given_name_initials
+        name = f"{individual[1]} {individual[0]}"
+        names.append(name.strip())
+    individuals = names
+
+    authors = individuals + organizations
+    if len(authors) > 0:
+        if len(authors) > 1:
+            authors[-1] = f"and {authors[-1]}"
+        if len(authors) == 2:
+            stylized_authors = " ".join(authors)
+        elif len(authors) > 2:
+            stylized_authors = ", ".join(authors)
+        else:
+            author = authors[0].rstrip(".")
+            stylized_authors = author
+    else:
+        authors = positions
+        if len(authors) > 1:
+            authors[-1] = f"and {authors[-1]}"
+        if len(authors) == 2:
+            stylized_authors = " ".join(authors)
+        elif len(authors) > 2:
+            stylized_authors = ", ".join(authors)
+        else:
+            author = authors[0].rstrip(".")
+            stylized_authors = author
+
+    stylized["authors"] = stylized_authors
+    stylized["pub_year"] = f"({pub_year(citation['pubdate'])})"
+    stylized["title"] = f"{citation['title']}."
+    stylized["publisher"] = f"{citation['publisher']}."
+    stylized["doi"] = doi_url(citation["doi"])
+
+    return stylized
+
+
+def esip(citation: dict) -> dict:
+
+    stylized = dict()
+
+    now = (pendulum.now("UTC")).format("YYYY-MM-DD", formatter="alternative")
+
+    individuals = _individuals(citation["authors"])
+    organizations = _organization(citation["authors"])
+    positions = _positions(citation["authors"])
+
+    names = list()
+    for individual in individuals:
+        given_name_initials = initials(individual[0])
+        individual[0] = given_name_initials
+        name = f"{individual[0]} {individual[1]}"
+        names.append(name.strip())
+    individuals = names
+
+    # Reverse lead author name
+    if len(individuals) > 0:
+        name = individuals[0].split(" ")
+        if len(name) == 2:
+            individuals[0] = f"{name[1]}, {name[0]}"
+
+    authors = individuals + organizations
+    if len(authors) > 0:
+        if len(authors) > 1:
+            authors[-1] = f"and {authors[-1]}"
+        if len(authors) == 2:
+            stylized_authors = " ".join(authors)
+        elif len(authors) > 2:
+            stylized_authors = ", ".join(authors)
+        else:
+            author = authors[0].rstrip(".")
+            stylized_authors = author
+    else:
+        # Only add positions if no individuals or organizations
+        authors = positions
+        if len(authors) > 1:
+            authors[-1] = f"and {authors[-1]}"
+        if len(authors) == 2:
+            stylized_authors = " ".join(authors)
+        elif len(authors) > 2:
+            stylized_authors = ", ".join(authors)
+        else:
+            author = authors[0].rstrip(".")
+            stylized_authors = author
+
+    stylized["authors"] = f"{stylized_authors}."
+    stylized["pub_year"] = f"{pub_year(citation['pubdate'])}."
+    stylized["title"] = f"{citation['title']}"
+    stylized["version"] = f"ver {citation['version']}."
+    stylized["publisher"] = f"{citation['publisher']}."
+    stylized["doi"] = f"{doi_url(citation['doi'])}."
+    stylized["accessed"] = f"Accessed {now}."
+
+    return stylized
+
+
+styles = {
+    "ESIP": esip,
+    "DRYAD": dryad
+}
+
