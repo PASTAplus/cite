39a63db819f205230df1d1b223532695c0a6d622
Mark Servilla <mark.servilla@gmail.com>
2020-02-04 10:20:59 -0700

    Add exceptions: DataPackageError and PastaEnvironmentError

--- webapp/exceptions.py
+++ webapp/exceptions.py
@@ -13,8 +13,16 @@
     1/27/20
 """
 
-class StyleError(Exception):
-    """Raised when a citation style error occurs
+
+class DataPackageError(Exception):
+    """Raised when a requested PASTA data package does not exist
+    Args:
+        msg (str): explanation of the error
+    """
+
+
+class PastaEnvironmentError(Exception):
+    """Raised when a requested PASTA environment does not exist
     Args:
         msg (str): explanation of the error
     """
@@ -22,6 +30,13 @@ class StyleError(Exception):
 
 class FormatError(Exception):
     """Raised when a citation format error occurs
+    Args:
+        msg (str): explanation of the error
+    """
+
+
+class StyleError(Exception):
+    """Raised when a citation style error occurs
     Args:
         msg (str): explanation of the error
     """
