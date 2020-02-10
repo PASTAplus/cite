bb9918dcb7700d0681bceb2cb182683028465ec5
Mark Servilla <mark.servilla@gmail.com>
2020-02-09 20:35:37 -0700

    Build author list on all EML creator elements and preserve order

--- webapp/style.py
+++ webapp/style.py
@@ -47,71 +47,52 @@ class Stylizer(object):
             raise StyleError(msg)
 
 
-def _individuals(authors: list) -> list:
-    individuals = list()
+def _names(authors: list) -> list:
+    """_names
+
+    Build a list of author names using the following logic:
+        1. Use individual(s), ignore all organization or position names
+           in creator element; otherwise
+        2. Use organization(s), ignore all position names in creator element;
+           otherwise
+        3. Use position(s)
+    :param authors: list of authors from citation data
+    :return: list of names in the form [[list], str]
+    """
+    names = list()
     for author in authors:
         individual_names = author["individual_names"]
-        for individual_name in individual_names:
-            given_names = individual_name["given_names"]
-            sur_name = individual_name["sur_name"]
-            name = [given_names, sur_name]
-            individuals.append(name)
-    return individuals
-
-
-def _organization(authors: list) -> list:
-    organizations = list()
-    for author in authors:
-        individual_names = author["individual_names"]
-        position_names = author["position_names"]
         organization_names = author["organization_names"]
-        if len(individual_names) == 0:
+        position_names = author["position_names"]
+        if len(individual_names) != 0:
+            for individual_name in individual_names:
+                given_names = individual_name["given_names"]
+                sur_name = individual_name["sur_name"]
+                name = [given_names, sur_name]
+                names.append(name)
+        elif len(organization_names) != 0:
+            organization_names = author["organization_names"]
             for organization_name in organization_names:
-                organizations.append(organization_name)
-    return organizations
-
-
-def _positions(authors: list) -> list:
-    positions = list()
-    for author in authors:
-        individual_names = author["individual_names"]
-        position_names = author["position_names"]
-        organization_names = author["organization_names"]
-        if len(individual_names) == 0 and len(organization_names) == 0:
+                name = [[], organization_name]
+                names.append(name)
+        else:
             for position_name in position_names:
-                positions.append(position_name)
-    return positions
+                name = [[], position_name]
+                names.append(name)
+    return names
 
 
 def bibtex(pid: str, access: str, citation: dict) -> dict:
 
     bibtext = dict()
 
-    individuals = _individuals(citation["authors"])
-    organizations = _organization(citation["authors"])
-    positions = _positions(citation["authors"])
-
-    names = list()
-    for individual in individuals:
-        individual[0] = " ".join([_.strip() for _ in individual[0]])
-        name = f"{individual[0]} {individual[1]}"
-        names.append(name.strip())
-    individuals = names
-
-    names = []
-    for organization in organizations:
-        names.append(f"{{{organization}}}")
-    organizations = names
-
-    authors = individuals + organizations
-    if len(authors) > 0:
-        stylized_authors = " and ".join(authors)
-    else:
-        names = []
-        for position in positions:
-            names.append(f"{{{position}}}")
-        authors = names
-        stylized_authors = " and".join(authors)
+    authors = list()
+    names = _names(citation["authors"])
+    for name in names:
+        _ = " ".join([_.strip() for _ in name[0]])
+        author = f"{_} {name[1]}"
+        authors.append(author.strip())
+    stylized_authors = " and ".join(authors)
 
     doi = doi_url(citation['doi'])
     publisher = citation['publisher']
@@ -143,31 +124,13 @@ def bibtex_online(pid: str, access: str, citation: dict) -> dict:
 
     bibtext = dict()
 
-    individuals = _individuals(citation["authors"])
-    organizations = _organization(citation["authors"])
-    positions = _positions(citation["authors"])
-
-    names = list()
-    for individual in individuals:
-        individual[0] = " ".join([_.strip() for _ in individual[0]])
-        name = f"{individual[0]} {individual[1]}"
-        names.append(name.strip())
-    individuals = names
-
-    names = []
-    for organization in organizations:
-        names.append(f"{{{organization}}}")
-    organizations = names
-
-    authors = individuals + organizations
-    if len(authors) > 0:
-        stylized_authors = " and ".join(authors)
-    else:
-        names = []
-        for position in positions:
-            names.append(f"{{{position}}}")
-        authors = names
-        stylized_authors = " and".join(authors)
+    authors = list()
+    names = _names(citation["authors"])
+    for name in names:
+        _ = " ".join([_.strip() for _ in name[0]])
+        author = f"{_} {name[1]}"
+        authors.append(author.strip())
+    stylized_authors = " and ".join(authors)
 
     doi = doi_url(citation['doi'])
     publisher = citation['publisher']
@@ -202,40 +165,21 @@ def dryad(citation: dict) -> dict:
 
     stylized = dict()
 
-    individuals = _individuals(citation["authors"])
-    organizations = _organization(citation["authors"])
-    positions = _positions(citation["authors"])
+    authors = list()
+    names = _names(citation["authors"])
+    for name in names:
+        # Make initials if given names present
+        _initials = initials(name[0], dot=False)
+        _ = f"{name[1]} {_initials}"
+        authors.append(_.strip())
 
-    names = list()
-    for individual in individuals:
-        given_name_initials = initials(individual[0], dot=False)
-        individual[0] = given_name_initials
-        name = f"{individual[1]} {individual[0]}"
-        names.append(name.strip())
-    individuals = names
+    if len(authors) > 1:
+        authors[-1] = f"and {authors[-1]}"
 
-    authors = individuals + organizations
-    if len(authors) > 0:
-        if len(authors) > 1:
-            authors[-1] = f"and {authors[-1]}"
-        if len(authors) == 2:
-            stylized_authors = " ".join(authors)
-        elif len(authors) > 2:
-            stylized_authors = ", ".join(authors)
-        else:
-            author = authors[0].rstrip(".")
-            stylized_authors = author
-    else:
-        authors = positions
-        if len(authors) > 1:
-            authors[-1] = f"and {authors[-1]}"
-        if len(authors) == 2:
-            stylized_authors = " ".join(authors)
-        elif len(authors) > 2:
-            stylized_authors = ", ".join(authors)
-        else:
-            author = authors[0].rstrip(".")
-            stylized_authors = author
+    if len(authors) == 2:
+        stylized_authors = " ".join(authors)
+    elif len(authors) > 2:
+        stylized_authors = ", ".join(authors)
 
     stylized["authors"] = stylized_authors
     stylized["pub_year"] = f"({pub_year(citation['pubdate'])})"
@@ -250,47 +194,30 @@ def esip(access: str, citation: dict) -> dict:
 
     stylized = dict()
 
-    individuals = _individuals(citation["authors"])
-    organizations = _organization(citation["authors"])
-    positions = _positions(citation["authors"])
+    authors = list()
+    names = _names(citation["authors"])
+    for name in names:
+        # Make initials if given names present
+        _initials = initials(name[0], dot=True)
+        _ = f"{_initials} {name[1]}"
+        authors.append(_.strip())
 
-    names = list()
-    for individual in individuals:
-        given_name_initials = initials(individual[0])
-        individual[0] = given_name_initials
-        name = f"{individual[0]} {individual[1]}"
-        names.append(name.strip())
-    individuals = names
-
-    # Reverse lead author name
-    if len(individuals) > 0:
-        name = individuals[0].split(" ")
-        if len(name) == 2:
-            individuals[0] = f"{name[1]}, {name[0]}"
-
-    authors = individuals + organizations
+    # Reverse lead author name if given name present
     if len(authors) > 0:
-        if len(authors) > 1:
-            authors[-1] = f"and {authors[-1]}"
-        if len(authors) == 2:
-            stylized_authors = " ".join(authors)
-        elif len(authors) > 2:
-            stylized_authors = ", ".join(authors)
-        else:
-            author = authors[0].rstrip(".")
-            stylized_authors = author
+        name = authors[0].split(" ")
+        if len(name) == 2:
+            authors[0] = f"{name[1]}, {name[0]}"
+
+    if len(authors) > 1:
+        authors[-1] = f"and {authors[-1]}"
+
+    if len(authors) == 2:
+        stylized_authors = " ".join(authors)
+    elif len(authors) > 2:
+        stylized_authors = ", ".join(authors)
     else:
-        # Only add positions if no individuals or organizations
-        authors = positions
-        if len(authors) > 1:
-            authors[-1] = f"and {authors[-1]}"
-        if len(authors) == 2:
-            stylized_authors = " ".join(authors)
-        elif len(authors) > 2:
-            stylized_authors = ", ".join(authors)
-        else:
-            author = authors[0].rstrip(".")
-            stylized_authors = author
+        author = authors[0].rstrip(".")
+        stylized_authors = author
 
     stylized["authors"] = f"{stylized_authors}."
     stylized["pub_year"] = f"{pub_year(citation['pubdate'])}."
